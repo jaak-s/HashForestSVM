4e28dd74e68b2167720bd6bb71b02c30e565a467
Jaak Simm <jaak.simm@gmail.com>
2014-09-19 20:00:46 +0200

    P08684 test

--- HashForestSVM/exampleSparse.py
+++ HashForestSVM/exampleSparse.py
@@ -26,6 +26,22 @@ def generateY(X, w):
     Y = [math.copysign(1,w.inner(x)) for x in X]
     return Y
 
+def readY(filename):
+    y = dict()
+    with open(filename) as f:
+        for line in f:
+            comp, activity = line.split()
+            y[int(comp)] = 2*int(activity) - 1
+    return y
+
+def readX(filename):
+    X = dict()
+    with open(filename) as f:
+        for line in f:
+            comp, inch, fp = line.split()
+            X[int(comp)] = {int(i): 1 for i in fp.split(",")}
+    return X
+
 def run():
     Xtrain = generateX(4000)
     #w      = generateW()
@@ -45,5 +61,22 @@ def run():
             acc += 1
     acc = acc / float(len(Ytest))
     print("Accuracy: %.3f" % acc)
+
+def run_P08684():
+    X = readX("/home/jaak/Dropbox/two/chemogen/P08684/P08684.1000.compounds.txt")
+    Y = readY("/home/jaak/Dropbox/two/chemogen/P08684/P08684.1000.train")
+
+    ids = Y.keys()
+    Ytrain = [Y[i] for i in ids]
+    Xtrain = [X[i] for i in ids]
+
+    lp = LinearPegasos(1.0, 100000)
+    lp.fit(Xtrain, Ytrain)
+
+    ## predict
+    Yhat = lp.decisionValues(Xtrain)
+    return ids, Yhat
+
+
 
 
